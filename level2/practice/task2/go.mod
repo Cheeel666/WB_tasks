1f331c3078bcdb16e2f5354c8e2db2fff4b84401
Ilya <ilchel1992@gmail.com>
2021-08-23 21:17:34 +0300

    added second task

---
+++ level2/practice/task2/go.mod
@@ -0,0 +1,3 @@
+module github.com/Cheeel666/WB_tasks/level2/practice/task2
+
+go 1.15
--- level2/practice/task2/unpack_str.go
+++ level2/practice/task2/unpack_str.go
@@ -1,15 +1,7 @@
-package main
-
-// import (
-// 	"level2/patterns"
-// 	"level2/theory"
-// import t "github.com/Cheeel666/WB_tasks/level2/practice/task1"
-
-// )
+package task2
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 )
 
@@ -25,8 +17,6 @@ func multChar(char string, n int) string {
 // UnpackStr unpacks string
 func UnpackStr(str string) (string, error) {
 	var result string
-	var flag bool
-	flag = false
 
 	if len(str) == 0 {
 		return "", nil
@@ -47,37 +37,14 @@ func UnpackStr(str string) (string, error) {
 
 	for i := 0; i < len(strRunes); i++ {
 		n, err := strconv.Atoi(string(strRunes[i]))
-		if rune(str[i]) == '\\' {
-			flag = true
-		}
-		if err == nil || flag {
+
+		if err == nil {
 			result += multChar(string(currentChar), n-1)
 			currentChar = strRunes[i+1]
-			flag = false
 		} else {
 			result += string(strRunes[i])
 			currentChar = strRunes[i]
 		}
 	}
 	return result, nil
-}
-func main() {
-	// patterns.Facade()
-	// patterns.BuilderPattern()
-	// patterns.VisitorPattern()
-	// patterns.CommandPattern()
-	// patterns.ChainOfResponsibilityPattern()
-	// patterns.FactoryMethodPattern()
-	// patterns.StrategyPattern()
-	// patterns.StatePattern()
-
-	// theory.Task1()
-	// theory.Task2()
-	// theory.Task3()
-	// theory.Task4()
-	// theory.Task5()
-	// theory.Task6()
-	// theory.Task7()
-	// t.TimeNow("")
-	fmt.Println(UnpackStr("qwe\4\5"))
 }
